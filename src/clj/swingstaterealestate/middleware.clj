eb9db3bb1a5cc67bfd3aebf2e90627632123dd12
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-05-19 10:14:21 -0700

    Whitelist GET requests only

--- src/clj/swingstaterealestate/middleware.clj
+++ src/clj/swingstaterealestate/middleware.clj
@@ -11,7 +11,7 @@
     (let [response (handler request)]
       (-> response
         (assoc-in [:headers "Access-Control-Allow-Origin"]  "*")
-        (assoc-in [:headers "Access-Control-Allow-Methods"] "GET,PUT,POST,DELETE,OPTIONS")
+        (assoc-in [:headers "Access-Control-Allow-Methods"] "GET")
         (assoc-in [:headers "Access-Control-Allow-Headers"] "X-Requested-With,Content-Type,Cache-Control")))))
 
 (defn wrap-base [handler]
