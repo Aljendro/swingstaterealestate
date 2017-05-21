8ca5c53db666a4f18551263d4f0c4d6dd3ab67b8
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-05-20 20:50:51 -0700

    Added minimum route tests

--- test/clj/swingstaterealestate/test/handler.clj
+++ test/clj/swingstaterealestate/test/handler.clj
@@ -4,10 +4,22 @@
             [swingstaterealestate.handler :refer :all]))
 
 (deftest test-app
-  (testing "main route"
-    (let [response ((app) (request :get "/"))]
-      (is (= 200 (:status response)))))
-
   (testing "not-found route"
     (let [response ((app) (request :get "/invalid"))]
-      (is (= 404 (:status response))))))
+      (is (= 404 (:status response)))))
+
+  (testing "county results endpoint"
+    (let [response ((app) (request :get "/api/results?state=CA"))]
+      (is (= 200 (:status response)))))
+
+  (testing "county results requires query parameter"
+    (let [response ((app) (request :get "/api/results"))]
+      (is (= 400 (:status response)))))
+
+  (testing "housing results endpoint"
+    (let [response ((app) (request :get "/api/housing?county=Orange&state=CA"))]
+      (is (= 200 (:status response)))))
+
+  (testing "housing results must have query params"
+    (let [response ((app) (request :get "/api/housing"))]
+      (is (= 400 (:status response))))))
